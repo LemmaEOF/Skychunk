f97838c954bdda762c41d1ccd2a6ce8281d4f7dc
Meredith Espinosa <Boundarybreaker@users.noreply.github.com>
2021-01-24 23:33:27 -0800

    Initial commit

---
+++ src/main/java/io/github/cottonmc/starter/FabricStarter.java
@@ -0,0 +1,15 @@
+package io.github.cottonmc.starter;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FabricStarter implements ModInitializer {
+	public static final String MODID = "starter";
+	public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
